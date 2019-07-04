69f42d72083c736644d026b0b1116abfd7bbe479
joaorodrigues2012 <37009151+joaorodrigues2012@users.noreply.github.com>
2019-07-04 01:50:57 -0300

    Inserindo campo de data e de upload

--- src/main/java/br/usjt/previsao_tempo/repository/PrevTempoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.usjt.previsao_tempo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.usjt.previsao_tempo.model.PrevTempo;
-
-public interface PrevTempoRepository extends JpaRepository<PrevTempo, Long> {
-
-}
